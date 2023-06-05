ec03a266db74e840ad52aa97e518f62910d2a900
Douglas Mendez <douglasmendez95@gmail.com>
2023-06-05 00:30:52 -0600

    cambio querys

--- src/database/querys.js
+++ src/database/querys.js
@@ -9,7 +9,7 @@ export const queriesComics = {
 }
 
 export const queriesPrestamos = {
-    obtenerPrestamos: 'select nombre, descripcion, estrellas,persona,fecha_prestamo from tbl_comics inner join tbl_prestamos on id=id_comic',
+    obtenerPrestamos: 'select nombre, descripcion, estrellas,id_comic,persona,fecha_prestamo from tbl_comics inner join tbl_prestamos on id=id_comic',
     agregarPrestamo:"INSERT INTO tbl_prestamos (id_comic,persona,fecha_prestamo) VALUES (@id,@persona,@fecha) UPDATE tbl_comics SET estado=@estado WHERE id=@id",
     eliminarPrestamo:"DELETE FROM tbl_prestamos where id_comic=@id UPDATE tbl_comics SET estado=@estado WHERE id=@id",
     validarPrestamo:"select count (*) from tbl_prestamos where id_comic=@id"
